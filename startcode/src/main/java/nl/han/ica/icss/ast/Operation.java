a1ba96e34ff927e087aba48ebc9d67a75c338a5e
Robin van der Vliet <info@robinvandervliet.com>
2021-04-01 19:40:43 +0200

    Store lhs and rhs in an array instead, so that we can have non-binary (unary, ternary, etc.) operations
    The functions getChildren and addChild work the same as before.

--- startcode/src/main/java/nl/han/ica/icss/ast/Operation.java
+++ startcode/src/main/java/nl/han/ica/icss/ast/Operation.java
@@ -1,29 +1,40 @@
 package nl.han.ica.icss.ast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
 
 public abstract class Operation extends Expression {
+    public Expression[] operands;
 
-    public Expression lhs;
-    public Expression rhs;
+    /**
+     * Create a new operation instance with a specific arity.
+     * @param arity The amount of expression parameters the operation has.
+     */
+    public Operation(int arity) {
+        this.operands = new Expression[arity];
+    }
 
     @Override
     public ArrayList<ASTNode> getChildren() {
         ArrayList<ASTNode> children = new ArrayList<>();
-        if(lhs != null)
-            children.add(lhs);
-        if(rhs != null)
-            children.add(rhs);
+
+        Arrays.stream(this.operands)
+                .filter(Objects::nonNull)
+                .forEach(children::add);
+
         return children;
     }
 
     @Override
     public ASTNode addChild(ASTNode child) {
-        if(lhs == null) {
-            lhs = (Expression) child;
-        } else if(rhs == null) {
-            rhs = (Expression) child;
+        for (int i = 0; i < this.operands.length; i++) {
+            if (this.operands[i] == null) {
+                this.operands[i] = (Expression) child;
+                break;
+            }
         }
+
         return this;
     }
 }
