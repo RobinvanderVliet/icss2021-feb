674d647b0de4aad45bd6213afb96bee4b8f75768
Robin van der Vliet <info@robinvandervliet.com>
2021-04-01 19:33:18 +0200

    Add missing removeChild method to Stylerule

--- startcode/src/main/java/nl/han/ica/icss/ast/Stylerule.java
+++ startcode/src/main/java/nl/han/ica/icss/ast/Stylerule.java
@@ -39,6 +39,18 @@ public class Stylerule extends ASTNode {
 
 		return this;
     }
+
+	@Override
+	public ASTNode removeChild(ASTNode child) {
+		if (child instanceof Selector) {
+			selectors.remove(child);
+		} else {
+			body.remove(child);
+		}
+
+		return this;
+	}
+
 	@Override
 	public boolean equals(Object o) {
 		if (this == o) return true;
