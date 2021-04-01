819860afe171c57b2676febc1158cf68042fb183
Robin van der Vliet <info@robinvandervliet.com>
2021-04-01 19:47:36 +0200

    Normalize colors in the constructor

--- startcode/src/main/java/nl/han/ica/icss/ast/literals/ColorLiteral.java
+++ startcode/src/main/java/nl/han/ica/icss/ast/literals/ColorLiteral.java
@@ -7,7 +7,26 @@ import java.util.Objects;
 public class ColorLiteral extends Literal {
     public String value;
 
+    /**
+     * Create an instance and try normalizing the color.
+     * Normalization happens by converting it to uppercase and by simplifying it if possible.
+     * This way we can easily compare #fff and #FFFFFF with the equal operator and still receive TRUE.
+     * @param value The color in hexadecimal format.
+     */
     public ColorLiteral(String value) {
+        value = value.toUpperCase();
+
+        //Simplify "#AABBCC" to "#ABC".
+        if (value.length() == 7 && value.startsWith("#")) {
+            if (
+                    value.substring(1, 2).equals(value.substring(2, 3)) &&
+                    value.substring(3, 4).equals(value.substring(4, 5)) &&
+                    value.substring(5, 6).equals(value.substring(6, 7))
+            ) {
+                value = "#" + value.substring(1, 2) + value.substring(3, 4) + value.substring(5, 6);
+            }
+        }
+
         this.value = value;
     }
     @Override
